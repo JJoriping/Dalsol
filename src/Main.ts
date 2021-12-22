9cf7b5a831b78062b6da61992657471c754ac54b
JJoriping <op@jjo.kr>
2021-12-22 23:47:25 +0900

    Add captcha system

--- src/Main.ts
+++ src/Main.ts
@@ -1,9 +1,13 @@
-import { Client, Intents, TextChannel } from "discord.js";
+import { Captcha } from "captcha-canvas";
+import { Client, Intents, MessageAttachment, TextChannel } from "discord.js";
+import randomColor from "randomcolor";
 import { getBasePreset } from "./components/BasePreset";
 import { getEmbedMessage } from "./components/EmbedMessage";
 import CREDENTIAL from "./data/credential.json";
 import SETTINGS from "./data/settings.json";
+import { DateUnit } from "./enums/DateUnit";
 import { Logger } from "./utils/Logger";
+import { randInt, sleep } from "./utils/Utility";
 
 const enum FooterStigma{
   GAME_ROLE = "게임 역할 받기"
@@ -12,11 +16,14 @@ const enum FooterStigma{
 const client = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
     Intents.FLAGS.GUILD_MESSAGES,
     Intents.FLAGS.GUILD_MESSAGE_REACTIONS
   ],
   retryLimit: 3
 });
+const captchaTable = new Map<string, [data:Captcha, channel:string, expired:number]>();
+
 async function main():Promise<void>{
   client.once('ready', async () => {
     const guild = await client.guilds.fetch(SETTINGS.guild);
@@ -40,10 +47,91 @@ async function main():Promise<void>{
     // process.exit(0);
     const logChannel = await client.channels.fetch(SETTINGS.logChannel);
     const roleChannel = await client.channels.fetch(SETTINGS.roleChannel);
+    const guestWelcomeChannel = await client.channels.fetch(SETTINGS.guestWelcomeChannel) as TextChannel;
     if(!logChannel?.isText()) throw Error(`Invalid logChannel: ${SETTINGS.logChannel}`);
     if(!roleChannel?.isText()) throw Error(`Invalid roleChannel: ${SETTINGS.roleChannel}`);
+    if(guestWelcomeChannel?.type !== "GUILD_TEXT") throw Error(`Invalid guestWelcomeChannel: ${SETTINGS.guestWelcomeChannel}`);
 
     await roleChannel.messages.fetch();
+    for(const v of (await guestWelcomeChannel.threads.fetch()).threads.values()){
+      await v.delete("부팅 시 존재하는 스레드 삭제");
+      await sleep(1);
+    }
+
+    client.on('guildMemberAdd', async member => {
+      Logger.info("Member Add").put(member.id).next("Tag").put(member.user.tag).out();
+      const thread = await guestWelcomeChannel.threads.create({
+        name: `${member.user.username} 님의 스레드`,
+        type: 'GUILD_PRIVATE_THREAD', // 부스트 2단계 이상이어야 한다.
+        autoArchiveDuration: 60,
+        reason: `${member.user.tag} 님에 대한 멤버십 심사`,
+        invitable: false
+      });
+      let message = await thread.send({
+        content: [
+          `<@${member.id}> 님, 달달소에 오신 걸 환영합니다 :wave:`,
+          `대화에 참여하시기 전 꼭 <#${SETTINGS.guestWelcomeChannel}> 채널의 모든 규칙을 읽고 지켜 주세요!`,
+          "준비가 되셨다면 제가 보내 드린 그림에서 선이 이어진 6글자를 입력해 주세요."
+        ].join('\n'),
+        files: [
+          new MessageAttachment(await registerCaptcha(member.id, thread.id), "captcha.png")
+        ]
+      });
+      const collector = thread.createMessageCollector({ time: DateUnit.HOUR });
+      let life = 5;
+
+      collector.on('collect', async v => {
+        if(v.author.id !== member.id) return;
+        const chunk = captchaTable.get(member.id);
+        if(!chunk) return;
+        Logger.log("Captcha").put(member.id)
+          .next("Input").put(v.content)
+          .next("Answer").put(chunk[0].text)
+          .out()
+        ;
+        if(chunk[0].text !== v.content){
+          await message.delete();
+          if(--life <= 0){
+            await member.kick("그림을 5회 연속으로 잘못 입력했습니다.");
+            return;
+          }
+          await registerCaptcha(member.id, thread.id);
+          message = await thread.send({
+            content: "다시 시도해 주세요!",
+            files: [
+              new MessageAttachment(await registerCaptcha(member.id, thread.id), "captcha.png")
+            ]
+          });
+          return;
+        }
+        collector.stop("인증 성공");
+        await v.reply({
+          embeds: [{
+            title: "✨ 입장 완료!",
+            color: 'GREEN',
+            description: "협조해 주셔서 감사합니다! 즐거운 달달소 여행 되세요 😉",
+            footer: {
+              text: "이 스레드는 1분 뒤 삭제됩니다."
+            }
+          }]
+        });
+        global.setTimeout(() => {
+          thread.delete("인증 성공");
+        }, DateUnit.MINUTE);
+        await member.roles.add(SETTINGS.regularRole);
+      });
+      collector.once('end', () => {
+        captchaTable.delete(member.id);
+      });
+    });
+    client.on('guildMemberRemove', async member => {
+      Logger.warning("Member Remove").put(member.id).next("Tag").put(member.user.tag).out();
+      const chunk = captchaTable.get(member.id);
+      if(!chunk) return;
+      const channel = await guestWelcomeChannel.threads.fetch(chunk[1]);
+      if(!channel) return;
+      channel.delete("유저 퇴장");
+    });
     client.on('messageCreate', async message => {
       if(message.channelId !== SETTINGS.roleChannel){
         if(message.channel.type === "GUILD_TEXT"
@@ -164,6 +252,24 @@ async function main():Promise<void>{
     Logger.success("Discord").put(client.user?.tag).out();
   });
   await client.login(CREDENTIAL.token);
+}
+async function registerCaptcha(userId:string, threadId:string):Promise<Buffer>{
+  if(captchaTable.get(userId)){
+    captchaTable.delete(userId);
+  }
+  const captcha = new Captcha(SETTINGS.captcha.width, SETTINGS.captcha.height);
+  const colors = [
+    randomColor(),
+    randomColor(),
+    randomColor()
+  ];
+  captcha
+    .addDecoy({ total: randInt(60, 80), color: colors[1], opacity: 0.8 })
+    .drawTrace({ size: randInt(4, 8), color: colors[0] })
+    .drawCaptcha({ size: 48, colors, characters: 6 })
+  ;
+  captchaTable.set(userId, [ captcha, threadId, Date.now() + DateUnit.HOUR ]);
+  return await captcha.png;
 }
 main();
 process.on('uncaughtException', e => {
